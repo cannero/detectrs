37b10238f153215bf1f2dc341efaf967ab7fabd1
Some One <someone@example.com>
2025-06-07 18:09:13 +0200

    added aws lambda runtime

--- README.md
+++ README.md
@@ -1,8 +1,18 @@
 # Detectrs
 Check for new mysql version and send a mail if something changed.
 ## Version
-### Netlify
-- use libsql and turso.tech
+started with Netlify, but no rust support
+### aws with third party libs
+- https://www.cargo-lambda.info/guide/getting-started.html
+- https://github.com/awslabs/aws-lambda-rust-runtime
+- use libsql (stack for debug must be extended) and turso.tech
 - mailtrap.io used, smtp2go.com needs domain name
+#### testing
+```bash
+cargo lambda watch
+# other shell
+cargo lambda invoke _ --data-ascii '{ "command": "hi" }'
+```
+
 ## other
 - https://github.com/dgtlmoon/changedetection.io
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use anyhow::{Result, anyhow};
 use entities::Notifier;
+use lambda_runtime::{Error, LambdaEvent, run, service_fn, tracing};
 use libsql_access::LibSqlAccess;
 use notifications::mailtrap_sender::MailtrapSender;
+use serde::Deserialize;
 use version_check::check_mysql_version;
 
 mod comparer;
@@ -13,12 +15,22 @@ mod parser;
 mod version_check;
 
 #[tokio::main]
-async fn main() -> Result<()> {
+async fn main() -> Result<(), Error> {
+    tracing::init_default_subscriber();
+
     match dotenvy::dotenv() {
         Ok(_) => println!("loading .env"),
         Err(_) => println!("no .env found"),
     }
 
+    run(service_fn(function_handler)).await
+}
+
+#[derive(Deserialize)]
+struct IncomingMessage {
+}
+
+async fn function_handler(_event: LambdaEvent<IncomingMessage>) -> Result<()> {
     let mailer = MailtrapSender::init()?;
     //let mailer = notifications::null_sender::NullSender{};
 
